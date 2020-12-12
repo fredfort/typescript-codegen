7b923f8b913d71c3db76c46cec0bbea9cdff0d81
Fred Fort <fred.fort@workday.com>
2020-12-12 19:56:30 +1100

    Kotlin API with Swagger

--- spring-boot-runtime/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ spring-boot-runtime/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,6 +2,11 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 @SpringBootApplication
 class DemoApplication
